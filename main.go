40e014243fa5dcce7b2bdae3913980c4cb7a8be0
Mihaita Alexandru Lupoiu <myhay.360@gmail.com>
2017-12-09 13:48:56 +0100

    Baisc form example.

--- main.go
+++ main.go
@@ -6,32 +6,32 @@ import (
 	"net/http"
 )
 
-// Todo is the structure of the tasks
-type Todo struct {
-	Title string
-	Done  bool
-}
-
-// TodoPageData is que structure of the page
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
+// ContactDetails is the structure of the contact.
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("layout.html"))
+	tmpl := template.Must(template.ParseFiles("forms.html"))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
 		}
-		tmpl.Execute(w, data)
-	})
 
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		// do something with details
+		_ = details
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
