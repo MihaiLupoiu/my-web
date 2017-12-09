4d0055ddf0565ab721237c655c81dec758528f55
Mihaita Alexandru Lupoiu <myhay.360@gmail.com>
2017-12-09 13:54:47 +0100

    Basic middleware.

--- main.go
+++ main.go
@@ -1,37 +1,28 @@
 package main
 
 import (
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-// ContactDetails is the structure of the contact.
-type ContactDetails struct {
-	Email   string
-	Subject string
-	Message string
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		f(w, r)
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "foo")
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "bar")
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("forms.html"))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-
-		details := ContactDetails{
-			Email:   r.FormValue("email"),
-			Subject: r.FormValue("subject"),
-			Message: r.FormValue("message"),
-		}
-
-		// do something with details
-		_ = details
-
-		tmpl.Execute(w, struct{ Success bool }{true})
-	})
+	http.HandleFunc("/foo", logging(foo))
+	http.HandleFunc("/bar", logging(bar))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
